8fa53154cfecc8615a2ba370a0364e91d78ff5af
Viraj Balasaheb Patil <95068416+Virajpatil818@users.noreply.github.com>
2023-01-10 21:59:11 +0530

    Update prg6Prims.c

--- All C Programs/prg6Prims.c
+++ All C Programs/prg6Prims.c
@@ -1,67 +1,45 @@
 #include <stdio.h>
-#include <stdbool.h>
-#include <limits.h>
 
-#define V 5
-
-int minkey(int key[],bool mstset[]){
-    int min = INT_MAX,min_index;
-
-    for(int v=0;v<V;v++){
-        if(mstset[v]==false && key[v]<min){
-            min=key[v],min_index=v;
-        }
-    }
-    return min_index;
-}
-
-int printMST(int parent[],int graph[V][V]){
-    int min_cost=0;
-    printf("Edge \tWeight\n");
-    for(int i=1;i<V;i++){
-        printf("%d- %d \t%d \n",parent[i],i,graph[i][parent[i]]);
-        min_cost+=graph[i][parent[i]];
-    }
-    printf("Minimum cost : %d\n",min_cost);
-}
-
-void primMST(int graph[V][V]){
-      // Array to store constructed MST
-    int parent[V];
-    // Key values used to pick minimum weight edge in cut
-    int key[V];
-    // To represent set of vertices included in MST
-    bool mstset[V];
-
-    for(int i=0;i<V;i++){
-        key[i]=INT_MAX , mstset[i]=false;
-    } 
-    // Always include first 1st vertex in MST.
-    // Make key 0 so that this vertex is picked as first
-    // vertex.  
-    key[0]=0;
-    parent[0]=-1;// First node is always root of MST
-    
-    for(int count=0;count<V-1;count++){
-        int u=minkey(key,mstset);
-        mstset[u]=true;
-
-        for(int v=0;v<V;v++){
-            if(graph[u][v] && mstset[v]==false && graph[u][v] <key[v]){
-                parent[v]=u,key[v]=graph[u][v];
+void main(){
+    int graph[10][10];
+    int visited[10]={0};
+    int n,cost=0,k=1,a,b,u,v,min,i,j;
+    printf("enter node \n");
+    scanf("%d",&n);
+    printf("Enter adjacency matrix : \n");
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            scanf(" %d",&graph[i][j]);
+            if(graph[i][j]==0){
+                graph[i][j]=999;
             }
         }
     }
-    printMST(parent,graph);
-}
+ 
+    visited[1]=1;
+    printf("\n");
 
-void main(){
-    int graph[V][V];
-    printf("Enter Adjecency Matrix\n");
-    for(int i=0;i<V;i++){
-        for(int j=0;j<V;j++){
-            scanf(" %d",&graph[i][j]);
+    while(k<n){
+        for(i=1,min=999;i<=n;i++){
+            for(j=1;j<=n;j++){
+                if(graph[i][j]<min){
+                    if(visited[i]!=0){
+                        min=graph[i][j];
+                        a=u=i;
+                        b=v=j;
+                    }
+                }
+            }
         }
+
+        if(visited[u]==0 || visited[v]==0){
+            printf("\nEdge %d:(%d %d) cost : %d",k++,a,b,min);
+            cost+=min;
+            visited[b]=1;
+        }
+        graph[a][b]=graph[b][a]=999;
     }
-    primMST(graph);
+
+    printf("\nminimum cost : %d",cost);
+
 }
